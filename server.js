f89cbcdbde7cf8225cd82248820899aaaeb20063
Tony <56505654+purr-purr@users.noreply.github.com>
2023-07-30 19:50:16 +0100

    fix: server settings

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 const port = 5000;
 // app.use(cors());
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const address = isProduction ? 'http://31.222.235.16' : 'http://localhost:3000';
+// const address = isProduction ? 'http://31.222.235.16' : 'http://localhost:3000';
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', address);
+	res.setHeader('Access-Control-Allow-Origin', 'http://31.222.235.16:4400');
 	next();
 });
 
