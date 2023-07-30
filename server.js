06a3224638dff7fae8117e9690f776dd66e2689d
Tony <56505654+purr-purr@users.noreply.github.com>
2023-07-30 19:32:32 +0100

    fix: server settings

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const path = require('path');
 
 const app = express();
 const port = 5000;
-app.use(cors());
+// app.use(cors());
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const address = isProduction ? 'http://31.222.235.16:4400' : 'http://localhost:3000';
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', address);
+	next();
+});
 
 app.get('/filenames/:folderName', (req, res) => {
 	const imagesPath = 'public/assets/property/' + req.params.folderName;
