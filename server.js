d3c96fddb9660f3467a0f7368d4f874e30c5b34d
Tony <56505654+purr-purr@users.noreply.github.com>
2023-08-20 14:18:17 +0100

    fix: domain IP

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 const isProduction = app.get('env') === 'production';
-const IP = isProduction ? 'http://akula.in.ua' : 'http://localhost:3000';
+const IP = isProduction ? 'http://31.222.235.16' : 'http://localhost:3000';
 
 require('dotenv').config();
 
