f28af07f3b0837a0bab23141191aa9b28daff7cb
Tony <56505654+purr-purr@users.noreply.github.com>
2023-10-01 21:54:12 +0100

    fix: currency api endpoint

--- server.js
+++ server.js
@@ -52,31 +52,32 @@ app.get('/data', (req, res) => {
 	dataRequest('real_estate', req, res);
 });
 
-app.get('/currency', async (req, res) => {
-	try {
-		const response = await axios.get(currencyRateUrl);
+app.get('/currency', (req, res) => {
+	axios
+		.get(currencyRateUrl)
+		.then((response) => {
+			if (response.status === 200) {
+				const html = response.data;
+				const $ = cheerio.load(html);
 
-		if (response.status === 200) {
-			const html = response.data;
-			const $ = cheerio.load(html);
+				const elementTable = $('.styled-table tr td').text();
+				const elementDate = $('#depoList h5').text();
 
-			const elementTable = $('.styled-table tr td').text();
-			const elementDate = $('#depoList h5').text();
-
-			const result = {
-				table: elementTable,
-				date: elementDate,
-			};
-			res.json(result);
-		} else {
-			const errorText = 'Failed to fetch the webpage www.eximb.com';
-			console.error(errorText);
-			res.status(500).send(errorText);
-		}
-	} catch (error) {
-		console.error('Error:', error);
-		res.status(500).send('An error occurred');
-	}
+				const result = {
+					table: elementTable,
+					date: elementDate,
+				};
+				res.json(result);
+			} else {
+				const errorText = 'Failed to fetch the webpage';
+				console.error(errorText);
+				res.status(500).send(errorText);
+			}
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+			res.status(500).send('An error occurred');
+		});
 });
 
 app.listen(port, () => {
