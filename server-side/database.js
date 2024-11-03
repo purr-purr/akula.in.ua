66957d78c3eba7a2bc27f9ffa76bb4075592a626
Tony <56505654+purr-purr@users.noreply.github.com>
2024-11-03 13:12:02 +0000

    fix: db connection

--- server-side/database.js
+++ server-side/database.js
@@ -3,24 +3,34 @@ import mysql from 'mysql';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
+	connectionLimit: 20,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_TABLE_NAME,
 });
 
-db.connect();
-
 const databaseConnection = (res) => {
 	const query = 'SELECT * FROM real_estate';
-	db.query(query, (error, results) => {
-		if (error) {
-			console.error(error);
-			res.status(500).json({error: 'Database query error'});
-		} else {
-			res.json(results);
+	
+	pool.getConnection((err, connection) => {
+		if (err) {
+			console.error('Error getting database connection:', err);
+			res.status(500).json({error: 'Database connection error'});
+			return;
 		}
+		
+		connection.query(query, (error, results) => {
+			connection.release();
+			
+			if (error) {
+				console.error('Database query error:', error);
+				res.status(500).json({error: 'Database query error'});
+			} else {
+				res.json(results);
+			}
+		});
 	});
 };
 
