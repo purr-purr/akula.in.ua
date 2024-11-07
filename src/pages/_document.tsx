9b8b8276fd4151227e3e64f41a94f039da717abe
Tony <56505654+purr-purr@users.noreply.github.com>
2024-11-07 15:54:49 +0000

    feat: add google tag manager and new property photo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import {Head, Html, Main, NextScript} from 'next/document';
 
-import { IS_PRODUCTION } from '@utils/const';
-import { GOOGLE_SERVICES } from '@utils/credentials';
+import {IS_PRODUCTION} from '@utils/const';
+import {GOOGLE_SERVICES} from '@utils/credentials';
 
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.googleapis.com"/>
 				<link
 					rel="preconnect"
 					href="https://fonts.gstatic.com"
@@ -23,6 +23,7 @@ export default function Document() {
 							async
 							src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_SERVICES.GA_TRACKING_ID}`}
 						/>
+
 						<script
 							dangerouslySetInnerHTML={{
 								__html: `
@@ -35,12 +36,31 @@ export default function Document() {
                                   `,
 							}}
 						/>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GOOGLE_SERVICES.GA_TAG_MANAGER_ID}');`,
+							}}
+						/>
 					</>
 				)}
 			</Head>
 			<body>
-				<Main />
-				<NextScript />
+			{/* Google Tag Manager (noscript) */}
+			<noscript>
+				<iframe
+					src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_SERVICES.GA_TAG_MANAGER_ID}`}
+					height="0"
+					width="0"
+					style={{display: 'none', visibility: 'hidden'}}
+				></iframe>
+			</noscript>
+			{/* End Google Tag Manager (noscript) */}
+			<Main/>
+			<NextScript/>
 			</body>
 		</Html>
 	);
